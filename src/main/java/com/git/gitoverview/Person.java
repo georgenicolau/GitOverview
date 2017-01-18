e4c47423cef834b20bdd02b0823407757efa1529
georgenicolau <pollercz@gmail.com>
2017-01-18 16:46:56 +0100

    Adding class Person

---
+++ src/main/java/com/git/gitoverview/Person.java
@@ -0,0 +1,34 @@
+package com.git.gitoverview;
+
+/**
+ * Created by gheorghe.nicolau on 1/18/2017.
+ */
+public class Person extends BaseEntity{
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
