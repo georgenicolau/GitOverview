de5d519078a8e112bba3a6550beaa3b4d06f7a59
georgenicolau <pollercz@gmail.com>
2017-01-18 16:32:53 +0100

    Adding new class BaseEntity

---
+++ src/main/java/com/git/gitoverview/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.git.gitoverview;
+
+/**
+ * Created by gheorghe.nicolau on 1/18/2017.
+ */
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
